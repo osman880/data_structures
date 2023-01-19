cdad68a243d297fe74521e979a951e1ab9597d41
abdullah.sagir <abdullah.sagir@BLG-BB06849.tbtk.gov.tr>
2023-01-19 16:43:17 +0300

    All functions in linked_list/readme.md were written.

--- linked_list/linked_list.h
+++ linked_list/linked_list.h
@@ -1,17 +1,35 @@
 #ifndef LINKEDLIST_H
 #define LINKEDLIST_H
 
-struct node;
-struct node_d;
-void data_to_linkedlist(struct node **i, int x);
-void printLinkedlist(struct node *ptr);
 
-void printdbList(struct node_d* head);
-void add_to_front(struct node** head, int data);
-void deleteLastNode(struct node **head);
-void addToBeginning(struct node_d** head, int data);
-void addToEnd(struct node_d **head, int data);
+/*slinked = single linked list
+ *dlinked = doubly linked list
+ *clinked = circular linked list */
 
+
+typedef struct node Node;
+typedef struct node_d Node_d;
+
+void print_slinked(Node *ptr);
+void print_dlinked(Node_d* head);
+void print_clinked(Node* circular_head);
+
+
+void data_to_end_slinked(Node **i, int x);
+void data_to_front_slinked(Node** head, int data);
+void delete_end_slinked(Node **head);
+void delete_front_slinked(Node **head_ref);
+int max_value_slinked(Node *head);
+int min_value_slinked(Node *head);
+
+
+void data_to_front_dlinked(Node_d** head, int data);
+void data_to_end_dlinked(Node_d **head, int data);
+void delete_front_dlinked(Node_d **head);
+void delete_end_dlinked(Node_d **head);
+void data_to_front_clinked(Node** head, int data);
+void data_to_end_clinked(Node** head, int data);
+void delete_node_clinked(Node** head, int data);
 
 
 
